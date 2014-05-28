1e727a1aca71d9b8c4183c04916deebc44210c93
George Yates III <me@georgeyatesiii.com>
2014-05-28 04:50:36 -0400

    Initial scaffolding commit

---
+++ crowdskout.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Plugin Name: Crowdskout
+ * Plugin URI: http://crowdskout.com
+ * Description: Adds Crowdskout analytics to your site
+ * Version: 1.0
+ * Author: George Yates III
+ * Author URI: http://georgeyatesiii.com
+ * License: GPL2
+ * 
+ * 
+ * Copyright 2014  George Yates III  (email : me@georgeyatesiii.com)
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as 
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+if (!function_exists('cdskt_add_analytics_js')) {
+	/**
+	 * The main function that takes our javascript and
+	 * adds it to the footer of the application for tracking.
+	 */
+	function cdsks_add_analytics_js() {
+		$sourceId = 0; // @TODO
+		require 'partials/footer-js.php';
+	}
+	add_action('wp_footer', 'cdsks_add_analytics_js');
+}
+
+if (!function_exists('cdskt_add_settings')) {
+	/**
+	 * Registering our settings
+	 */
+	function cdskt_add_settings() {
+		register_settings('cdskt_plugin', 'sourceId');
+
+	}
+	add_action('admin_init', 'cdskt_add_settings');
+}
