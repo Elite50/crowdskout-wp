74ed376d3773ea4cbf349f0800205553d3672e37
James Keary <jkeary@crowdskout.com>
2014-10-17 08:45:40 -0400

    file path fixes

--- shortcodes/email/email-shortcode.php
+++ shortcodes/email/email-shortcode.php
@@ -6,6 +6,6 @@ add_shortcode('cs_email', 'email_shortcode');
 
 function email_shortcode($attrs, $content = null) {
 	ob_start();
-	require CSKT_PLUGIN_SERVER_ROOT . '/email/email-tpl.php';
+	require CSKT_PLUGIN_SERVER_ROOT . '/shortcodes/email/email-tpl.php';
 	return ob_get_clean();
 }
--- shortcodes/like/like-shortcode.php
+++ shortcodes/like/like-shortcode.php
@@ -6,6 +6,6 @@ add_shortcode('cs_like', 'cs_like_handler');
 
 function cs_like_handler($attrs, $content = null) {
 	ob_start();
-	require CSKT_PLUGIN_SERVER_ROOT . '/like/like-tpl.php';
+	require CSKT_PLUGIN_SERVER_ROOT . '/shortcodes/like/like-tpl.php';
 	return ob_get_clean();
 }
