8bceac4cdaea5dafea56927ad903f270fe29494f
James Keary <jkeary@crowdskout.com>
2015-03-20 17:30:09 -0400

    oauth set up, access tokes, client and source ids, css for settings page

--- crowdskout.php
+++ crowdskout.php
@@ -52,14 +52,143 @@
         }
     }
 
+	if (!function_exists('cskt_add_admin_scripts')) {
+		add_action( 'admin_enqueue_scripts', 'mn_add_admin_scripts' );
+		function mn_add_admin_scripts() {
+			if (!WP_DEBUG) {
+				$flag = '.min';
+			} else {
+				$flag = '';
+			}
+			wp_enqueue_script('admin_scripts' . $flag . ".js", plugins_url() . "/crowdskout-wp/js/scripts_admin" . $flag . ".js", array('jquery'), '', true);
+			if (isset($_GET['page']) && $_GET['page'] == 'crowdskout') {
+				wp_enqueue_style( 'styles', plugins_url() . '/crowdskout-wp/css/styles_admin.css', '', false, '' );
+			}
+		}
+	}
+
     /**
      * The main function that takes cskts javascript and
      * adds it to the footer of the application for tracking.
      */
     if (!function_exists('cskt_add_analytics_js')) {
-
+	    add_action('wp_footer', 'cskt_add_analytics_js');
         function cskt_add_analytics_js() {
-            require CSKT_PLUGIN_SERVER_ROOT . '/views/footer-js.php';
+	        if (get_option('access_token')) {
+
+		        if (WP_DEBUG) {
+			        //$action  = "http://dev.api.crowdskout.com/oauth/tracking"; // George
+			        $action  = "http://loc.cs-back.com/oauth/tracking"; // Will
+		        } else {
+			        $action  = "https://api.crowdskout.com/oauth/tracking";
+		        }
+
+				$auth = "Bearer " . get_option('access_token');
+				$args = array(
+					'headers' => array (
+						"Authorization" => $auth
+					)
+				);
+
+				$response = wp_remote_get($action, $args);
+
+		        $substr=strchr($response['body'], "source");
+		        $exploded = explode('"', $substr);
+
+		        $keys = array();
+		        $values = array();
+		        for ($i=0; $i < sizeof($exploded); ++$i) {
+			        if (preg_match('!\d+!', $exploded[$i])) {
+				        preg_match_all('!\d+!', $exploded[$i], $matches);
+				        array_push($values, $matches[0][0]);
+			        } else {
+				        array_push($keys, $exploded[$i]);
+			        }
+		        }
+		        for ($x=0; $x<sizeof($keys);$x++) {
+			        if ($keys[$x] == "source" && !get_option('cskt_source_id')) {
+				        add_option('cskt_source_id', $values[$x]);
+			        }
+			        if ($keys[$x] == "client" && !get_option('cskt_client_id')) {
+				        add_option('cskt_client_id', $values[$x]);
+			        }
+		        }
+
+		        require CSKT_PLUGIN_SERVER_ROOT . '/views/footer-js.php';
+	        }
         }
     }
-    add_action('wp_footer', 'cskt_add_analytics_js');
+
+	if (!function_exists('cskt_oauth_connect')) {
+		add_action('init', 'cskt_oauth_connect');
+		/**
+		 *
+		 */
+		function cskt_oauth_connect() {
+
+			class OAuthConnect {
+				private $access_token;
+				public function __construct() {
+					// init variables
+					// get the endpoint/action
+					if (WP_DEBUG) {
+						//$action  = "http://dev.api.crowdskout.com/oauth/access_token"; // George
+						$action  = "http://loc.cs-back.com/oauth/access_token"; // Will
+					} else {
+						$action  = "https://api.crowdskout.com/oauth/access_token";
+					}
+
+					if(isset($_POST["submit"])) {
+						// form validation checking of user input
+						$body = array(
+							'body' => array (
+								'grant_type' => 'password',
+								'password' => $_POST["password"],
+								'username' => $_POST["cskt_account"],
+								// george
+//								'client_id' => 'abcd',
+//								'client_secret' => 'DPdANFHSZkukp82NB9yFLlX3ivF8LDys'
+								// will
+								'client_id' => 'r1NALiNIJN4qFCZoL6ThgarZUXuBhSVpDcDf9Ga',
+								'client_secret' => 'v2tVxJI9rkbUJlm5yE1pdXnr5YSgVIE6swwJPn3'
+							)
+						);
+						$response = wp_remote_post($action, $body);
+						var_dump($response);
+						$exploded = explode('"', $response['body']);
+						for ($i=0; $i < sizeof($exploded); ++$i) {
+							if ($exploded[$i] = 'access_token') {
+								$this->access_token = $exploded[$i+3];
+								// send at to db
+								add_option('access_token', $this->access_token);
+								var_dump($this->access_token);
+								break;
+							}
+						}
+					}
+				}
+
+				public function getAccessToken() {
+					return $this->access_token;
+				}
+
+			}
+			if (get_option('access_token')) {
+				return;
+			} else {
+				$oauth = new OAuthConnect();
+			}
+
+		}
+	}
+
+	// crowdskout wordpress db
+	if (!function_exists('cskt_create_database_table')) {
+		add_action('init', 'cskt_create_database_table');
+		/**
+		 *
+		 */
+		function cskt_create_database_table() {
+			global $wpdb;
+		}
+	}
--- views/admin-page.php
+++ views/admin-page.php
@@ -1,9 +1,20 @@
+<h1>Crowdskout</h1>
 <div class="wrap">
-	<?php screen_icon(); ?>
-	<h2>Crowdskout</h2>
-	<form method="post" action="options.php">
-		<?php settings_fields('cskt_plugin'); ?>
-		<?php do_settings_sections('crowdskout'); ?>
-		<?php submit_button(); ?>
+	<?php if (!get_option('access_token')) : ?>
+	<h1 class="login-logo"></h1>
+	<form class="cskt_connect" method="POST" action="">
+		<input type="text" name="cskt_email" class="cskt_email" placeholder="email" required/>
+		<input type="password" name="cskt_password" class="cskt_password" placeholder="password" required/>
+		<input type="submit" name="submit" value="Login"/>
+		<div id="submit_response"></div>
 	</form>
+
+	<form id="cskt_oauth" method="POST" action="">
+		<label>Choose account</label>
+		<select id="cskt_accounts" name="cskt_account">
+		</select>
+		<input type="submit" name="submit" value="connect" />
+	</form>
+	<?php endif; ?>
+
 </div>
